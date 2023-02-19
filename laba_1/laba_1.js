04b265a12c064fd4814368ab8c6bfe493cdfda20
Zudin Maxim <anonim010899@gmail.com>
2023-02-19 13:44:12 +0300

    add laba_1

--- laba_1/laba_1.js
+++ laba_1/laba_1.js
@@ -1,14 +1,14 @@
 let {Document} = require('docxyz');
 let fileName = 'variant04.docx';
 let document = new Document(fileName);
-let a = [];
 
 const RGBCheck = ({ r, g, b }) => {
     return Boolean(r && g && b)
 }
 
-let kod = '';
 
+let kod = '';
+let shifrText = [];
 for(let paragraph of document.paragraphs){
     for(let run of paragraph.runs) {
         const fontColor = RGBCheck(run.font.color.rgb);
@@ -16,16 +16,15 @@ for(let paragraph of document.paragraphs){
         const fontHighlightColor = run.font.highlight_color
         const fontScale = run._r.get_or_add_rPr().xpath("./w:w")[0]
         const fontSpacing = run._r.get_or_add_rPr().xpath("./w:spacing")[0]
-        // console.log(fontSpacing[0])
-        // console.log(run.text)
         if(
             fontColor ||
             (fontSize !== 12) ||
             (fontHighlightColor !== 8) ||
-            fontScale ||
+            fontScale ||    
             fontSpacing
             ) {
-
+            shifrText.push(run.text);
+            run.underline = true;
             for(let i = 0; i < run.text.length; i++) {
                 kod += '1'
             }
@@ -38,18 +37,26 @@ for(let paragraph of document.paragraphs){
     }
 }
 // kod += '000'
-
+console.log('<------------------------------------------------------------->')
 console.log(kod)
 console.log(kod.length)
+console.log('<------------------------------------------------------------->')
+console.log(shifrText)
 const byteCharacters = kod.match(/.{1,8}/g)
+console.log('<------------------------------------------------------------->')
 console.log(byteCharacters)
 const byteNumbers = byteCharacters.map(byte => parseInt(byte, 2))
+console.log('<------------------------------------------------------------->')
 console.log(byteNumbers)
 const byteArray = new Uint8Array(byteNumbers)
 const decoderWindows = new TextDecoder('windows-1251')
 const decoderKOI8 = new TextDecoder('KOI8-R')
 const decodercp866 = new TextDecoder('cp866')
 console.log('KOI8-R --- |', decoderKOI8.decode(byteArray), '|')
+console.log('<------------------------------------------------------------->')
 console.log('cp866 --- |', decodercp866.decode(byteArray), '|')
+console.log('<------------------------------------------------------------->')
 console.log('Windows-1251 --- |', decoderWindows.decode(byteArray), '|')
+console.log('<------------------------------------------------------------->')
 
+document.save(fileName)
